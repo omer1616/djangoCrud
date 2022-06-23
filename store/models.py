a83b6d0939ea92c016f5f2970b4c0dd0ec6e21a2
omer1616 <omerinci415@gmail.com>
2022-06-23 23:43:55 +0300

    added media

--- store/models.py
+++ store/models.py
@@ -18,7 +18,7 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.FloatField()
     digital = models.BooleanField(default=False, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
+    image = models.ImageField(null=True, blank=True, upload_to='product')
 
     def __str__(self):
         return self.name
