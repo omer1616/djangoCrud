7d4606657ef1ba0bb98097f5d39eb5ec7be2bfa5
omer1616 <omerinci415@gmail.com>
2022-06-26 19:32:57 +0300

    add product and order

--- store/views.py
+++ store/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import *
 
 
 # Create your views here.
@@ -8,12 +9,20 @@ def main(request):
 
 
 def card(request):
-    context = {}
-    return render(request, "store/card.html", context)
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    context = {'items': items}
+    return render(request, 'store/card.html')
 
 
 def store(request):
-    context = {}
+    products = Product.objects.all()
+
+    context = {
+        'products': products
+    }
     return render(request, "store/store.html", context)
 
 
