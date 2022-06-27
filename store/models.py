b27ec3849b7a796a3cbed91caf1a17701fd9bfe3
omer1616 <omerinci415@gmail.com>
2022-06-27 23:11:01 +0300

    added order item card

--- store/models.py
+++ store/models.py
@@ -4,11 +4,11 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-
 class Customer(models.Model):
-    user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
-    name = models.CharField(max_length=200)
-    email = models.EmailField(max_length=200, null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True, default='',
+                                related_name='customer')
+    name = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
@@ -18,7 +18,7 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.FloatField()
     digital = models.BooleanField(default=False, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True, upload_to='product')
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -41,12 +41,29 @@ class Order(models.Model):
     def __str__(self):
         return str(self.id)
 
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
+
 
 class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
 
 
 class ShippingAddress(models.Model):
