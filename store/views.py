d7d0e7eb3ddec217f0f2394af6b04bd75709c066
omer1616 <omerinci415@gmail.com>
2022-07-12 01:34:12 +0300

    added card.js

--- store/views.py
+++ store/views.py
@@ -14,6 +14,7 @@ def cart(request):
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
+
     else:
         # Create empty cart for now for non-logged in user
         items = []
