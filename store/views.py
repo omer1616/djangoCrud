e622f5613588f03196cbb433a375d2bb989fad99
omer1616 <omerinci415@gmail.com>
2022-07-13 19:02:47 +0300

    add shipping

--- store/views.py
+++ store/views.py
@@ -17,11 +17,11 @@ def cart(request):
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
-        cart_items =  order.get_cart_items
+        cart_items = order.get_cart_items
     else:
         # Create empty cart for now for non-logged in user
         items = []
-        order = {'get_cart_total': 0, 'get_cart_items': 0}
+        order = {'get_cart_total': 0, 'get_cart_items': 0, 'shipping': False}
         cart_items = order['get_cart_items']
 
     context = {'items': items, 'order': order, 'cart_items':  cart_items}
@@ -37,7 +37,7 @@ def store(request):
     else:
         # Create empty cart for now for non-logged in user
 
-        order = {'get_cart_total': 0, 'get_cart_items': 0}
+        order = {'get_cart_total': 0, 'get_cart_items': 0,'shipping': False}
         cart_items = order['get_cart_items']
     products = Product.objects.all()
     paginator = Paginator(products, 2)  # Show 25 contacts per page.
