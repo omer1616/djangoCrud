9e2406faffa0c0b6734da7bf23ff5e7fe9ec58a0
omer1616 <omerinci415@gmail.com>
2022-07-13 14:46:45 +0300

    added product bucket

--- store/views.py
+++ store/views.py
@@ -52,4 +52,19 @@ def update_item(request):
     print('product_id: ', product_id)
     print('action: ', action)
 
+    customer = request.user.customer
+    product = Product.objects.get(id=product_id)
+    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
+    order_item, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+    if action == 'add':
+        order_item.quantity = (order_item.quantity + 1)
+    elif action == 'remove':
+        order_item.quantity = (order_item.quantity - 1)
+
+    order_item.save()
+
+    if order_item.quantity <= 0:
+        order_item.delete()
     return JsonResponse("item was added", safe=False)
